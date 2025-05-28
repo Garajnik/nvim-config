eedf10daa33f9d825aa01627247b855d2e7ebdd8
Leonid (Garaj) <lexan1699@gmail.com>
2025-05-28 15:36:29 +0700

    123

--- lua/plugins/dash.lua
+++ lua/plugins/dash.lua
@@ -24,6 +24,7 @@ return {
         keys = {
           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            --{icon = "", key = "p", desc="Open Projects", action="cmd>Telescope projects<cr>"},
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
