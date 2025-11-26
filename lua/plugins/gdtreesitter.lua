e8f7f22a185b4bd963cd63a2101afbf8c507ebd1
Garajnik <lexan1699@gmail.com>
2025-11-26 20:59:24 +0700

    Add godot support

---
+++ lua/plugins/gdtreesitter.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "gdscript", "godot_resource", "gdshader" },
+
+      highlight = {
+        enable = true,
+      },
+
+      auto_install = false,
+
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
+  },
+}
