109683ba2e2c2c591eea0316336e31e4e1f5c6a1
Garajnik <lexan1699@gmail.com>
2026-09-18 19:56:31 +0700

    Delete unused plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,8 @@
 return {
   {
-    "loctvl842/monokai-pro.nvim",
-    lazy = false, -- Load immediately
-    priority = 1000, -- High priority to ensure it loads early
-    config = function()
-      require("monokai-pro").setup({
-        -- Optional: Configure filter (options: "default", "octagon", "pro", "machine", "ristretto", "classic", "spectrum")
-        filter = "pro",
-      })
-      vim.cmd.colorscheme("monokai-pro")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
   },
 }
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
